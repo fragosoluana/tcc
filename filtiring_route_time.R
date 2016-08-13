57042bc5d308a6b7e09225d3d41721ead20377ba
Luana Fragoso <luana.fragoso@webradar.com>
2016-08-13 15:24:46 -0300

    - Comandos gerados para testar a lógica com um aeroporto (SBBE) - Criacao do arquivo que será o script que aplicará a lógica para todos os aeroportos

---
+++ filtiring_route_time.R
@@ -0,0 +1,4 @@
+#ler todos os arquivos da pasta
+  #inputs: planilha com rotas e tempo médio; planilha com as regras
+#para cada arquivo aplicar a lógica
+#salvar arquivos resultantes (CSVs) numa pasta 
