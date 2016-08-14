07bab38214d244e473bae025c2a8fd02050bc1e4
Luana Fragoso <luana.fragoso@webradar.com>
2016-08-14 18:39:16 -0300

    - Importação dos CSVs para a pasta Sheets que contem as regras de cada aeroporto - Script para filtrar regras atualizado. Por enquanto so filtra em relaçao a rota e o tempo de viagem - CSV das rotas com suas medias de tempo de viagem

--- filtiring_route_time.R
+++
@@ -1,4 +0,0 @@
-#ler todos os arquivos da pasta
-  #inputs: planilha com rotas e tempo médio; planilha com as regras
-#para cada arquivo aplicar a lógica
-#salvar arquivos resultantes (CSVs) numa pasta 
