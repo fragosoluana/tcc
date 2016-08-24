101cef2dcfd070cd1c3e051deb49a875a1d05e92
Luana Fragoso <luana.fragoso@webradar.com>
2016-08-23 23:26:57 -0300

    - Atualizacao do script de filtragem de regras 	- Verificação dos lifts > 1 	- Verificação dos autolifts

--- filtiring_rules.R
+++ filtiring_rules.R
@@ -13,43 +13,88 @@ require(sqldf)
 require(stringr)
 
 #Lê o arquivo com a média do tempo das rotas
-route <- read.csv("route-time-avg.csv",
-                       header = TRUE,
-                       quote="\"",
-                       stringsAsFactors= TRUE,
-                       strip.white = TRUE)
+files <- list.files("rules", pattern="*.csv", full.names=TRUE)
 
-files <- list.files("Sheets", pattern="*.csv", full.names=TRUE)
+route <- read.csv("route-time-avg.csv",
+                  header = TRUE,
+                  quote="\"",
+                  stringsAsFactors= TRUE,
+                  strip.white = TRUE)
+
+route <- data.frame(departure = route$departure, arrival = route$arrival, 
+                    travel_time_avg = as.integer(substring(route$travel_time_avg, 1, 2)))
 
 for (i in seq_along(files)) {
-  
   #Lê o csv com as regras de cada aeroporto
-  rules <- read.csv(files[i],
-                       header = TRUE,
-                       quote="\"",
-                       stringsAsFactors= TRUE,
-                       strip.white = TRUE)
+  d_rules <- read.csv(files[i],
+                      header = TRUE,
+                      quote="\"",
+                      stringsAsFactors= TRUE,
+                      strip.white = TRUE)
   
-  rules <- str_match(rules$rules, ".(SB..)_.*(SB..)_(.).*")
-  rules <- data.frame(departure = rules[,2], arrival = rules[,3], delay_time = rules[,4])
+  re <- str_match(d_rules$rules, ".(SB..)_.=(.*)......(SB..)_(.)=(.*).")
   
-  route <- data.frame(departure = route$departure, arrival = route$arrival, 
-                           travel_time_avg = as.integer(substring(route$travel_time_avg, 1, 2)))
+  d_rules <- data.frame(departure = re[,2], d_intensity = re[,3], arrival = re[,4], a_intensity = re[,6], 
+                        delay_time = re[,5], lift = d_rules$lift)
   
-  #Compara se o tempo de atraso é maior do que o tempo de viagem e verifica se as rotas geradas nas regras realmente existem
-  result <- sqldf('SELECT rules.* 
-                     FROM rules 
-                     INNER JOIN route ON route.departure = rules.departure 
-                     AND route.arrival = rules.arrival 
-                     AND route.travel_time_avg < rules.delay_time')
+  d_rules <- sqldf('SELECT d_rules.* 
+                  FROM d_rules 
+                  INNER JOIN route 
+                  ON route.departure = d_rules.departure 
+                  AND route.arrival = d_rules.arrival 
+                  AND route.travel_time_avg < d_rules.delay_time
+                  AND d_rules.lift > 1')
   
-  #Ecreve a tabela resultante em um csv
-  subdir <- "results"
+  result <- data.frame(departure = character(), d_intensity = character(), arrival = character(), a_intensity = character(), 
+                       delay_time = character(), lift = character())
   
-  if (!file.exists(subdir)){
-    dir.create(file.path(getwd(), subdir))
+  arrivals <- unique(d_rules$arrival)
+  if(length(arrivals) != 0) {
+    for (i in seq_along(arrivals)) {
+      a_rules <- read.csv(paste("rules/", paste(arrivals[i], ".csv", sep=""), sep=""),
+                          header = TRUE,
+                          quote="\"",
+                          stringsAsFactors= TRUE,
+                          strip.white = TRUE)
+      
+      re <- str_match(a_rules$rules, ".(SB..)_.=(.*)......(SB..)_(.)=(.*).")
+      
+      a_rules <- data.frame(departure = re[,2], d_intensity = re[,3], arrival = re[,4], a_intensity = re[,6], 
+                            delay_time = re[,5], lift = a_rules$lift)
+      
+      autolift <- sqldf('SELECT a_rules.* 
+                      FROM a_rules 
+                      WHERE arrival = departure
+                      AND d_intensity = "high"
+                      AND a_intensity = "high"
+                      AND lift > 1')
+      
+      diff <- setdiff(c(1, 2, 3, 4), autolift$delay_time)
+      if(length(diff) != 0) {
+        for (i in seq_along(diff)) {
+          autolift <- rbind(autolift, data.frame(departure = a_rules[1,1], d_intensity = "high", arrival = a_rules[1,1], 
+                                                 a_intensity = "high", delay_time = diff[i], lift = 0))
+        }
+      }
+      
+      result <- rbind(result, sqldf('SELECT d_rules.* 
+                    FROM d_rules 
+                    INNER JOIN autolift
+                    ON d_rules.arrival = autolift.arrival
+                    AND d_rules.delay_time = autolift.delay_time
+                    AND d_rules.lift > autolift.lift'))
+    }	
   }
   
-  out_file <- paste(paste(subdir, "/", sep = ""), substr(files[i], 8, 11), sep = "")
-  write.csv(result, file = paste(out_file, ".csv", sep = ""), row.names=FALSE, na="")
+  if(length(result$departure) != 0) {
+    #Ecreve a tabela resultante em um csv
+    subdir <- "results"
+    
+    if (!file.exists(subdir)){
+      dir.create(file.path(getwd(), subdir))
+    }
+    
+    out_file <- paste(paste(subdir, "/", sep = ""), result[1,1], sep = "")
+    write.csv(result, file = paste(out_file, ".csv", sep = ""), row.names=FALSE, na="")
+  }
 }
